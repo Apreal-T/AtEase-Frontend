7ce1fad8d3e8bb22fa1f407e5d6fdfb17ac7f0e1
bisioluimoleayo <bisioluimoleayo1@gmail.com>
2021-12-23 15:19:34 +0100

    Making my page visible

---
+++ src/pages/Part-four/Partfour.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import {  } from 'react-bootstrap'
+import rectangle from "../../images/rectangle-background.png"
+
+function Partfour() {
+    return (
+        <Container>
+            <div className=''>
+                <img src={rectangle} alt='rectangle background'/>
+            </div>
+        </Container>
+    )
+}
+
+export default Partfour
